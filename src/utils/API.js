5b7056aaf915714993ab3402962610ba61a34ddd
Rajesh Sharma <sharma.rajesh2k1@gmail.com>
2026-06-26 01:30:38 +0530

    updated vercel api

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 // get logged in user's info 
 
 export const getMe = (token) => {
-  return fetch('http://localhost:3000/api/user/me', {
+  return fetch('https://fitness-express-app.vercel.app/api/user/me', {
     headers: {
       'Content-Type': 'application/json',
       authorization: `Bearer ${token}`,
@@ -10,7 +10,7 @@ export const getMe = (token) => {
 };
 
 export const createUser = (userData) => {
-  return fetch("http://localhost:3000/api/user", {
+  return fetch("https://fitness-express-app.vercel.app/api/user", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(userData),
@@ -18,7 +18,7 @@ export const createUser = (userData) => {
 };
 
 export const loginUser = (userData) => {
-  return fetch("http://localhost:3000/api/user/login", {
+  return fetch("https://fitness-express-app.vercel.app/api/user/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(userData),
@@ -26,7 +26,7 @@ export const loginUser = (userData) => {
 };
 
 export const createCardio = (cardioData, token) => {
-  return fetch("http://localhost:3000/api/exercise/cardio", {
+  return fetch("https://fitness-express-app.vercel.app/api/exercise/cardio", {
     method: "POST",
     headers: {
       'Content-Type': 'application/json',
@@ -37,7 +37,7 @@ export const createCardio = (cardioData, token) => {
 }
 
 export const createResistance = (resistanceData, token) => {
-  return fetch("http://localhost:3000/api/exercise/resistance", {
+  return fetch("https://fitness-express-app.vercel.app/api/exercise/resistance", {
     method: "POST",
     headers: {
       'Content-Type': 'application/json',
@@ -48,7 +48,7 @@ export const createResistance = (resistanceData, token) => {
 }
 
 export const getCardioById = (cardioId, token) => {
-  return fetch(`http://localhost:3000/api/exercise/cardio/${cardioId}`, {
+  return fetch(`https://fitness-express-app.vercel.app/api/exercise/cardio/${cardioId}`, {
     headers: {
       'Content-Type': 'application/json',
       authorization: `Bearer ${token}`,
@@ -57,7 +57,7 @@ export const getCardioById = (cardioId, token) => {
 }
 
 export const getResistanceById = (resistanceId, token) => {
-  return fetch(`http://localhost:3000/api/exercise/resistance/${resistanceId}`, {
+  return fetch(`https://fitness-express-app.vercel.app/api/exercise/resistance/${resistanceId}`, {
     headers: {
       'Content-Type': 'application/json',
       authorization: `Bearer ${token}`,
@@ -66,7 +66,7 @@ export const getResistanceById = (resistanceId, token) => {
 }
 
 export const deleteCardio = (cardioId, token) => {
-  return fetch(`http://localhost:3000/api/exercise/cardio/${cardioId}`, {
+  return fetch(`https://fitness-express-app.vercel.app/api/exercise/cardio/${cardioId}`, {
     method: "DELETE",
     headers: {
       authorization: `Bearer ${token}`,
@@ -75,7 +75,7 @@ export const deleteCardio = (cardioId, token) => {
 }
 
 export const deleteResistance = (resistanceId, token) => {
-  return fetch(`http://localhost:3000/api/exercise/resistance/${resistanceId}`, {
+  return fetch(`https://fitness-express-app.vercel.app/api/exercise/resistance/${resistanceId}`, {
     method: "DELETE",
     headers: {
       authorization: `Bearer ${token}`,
